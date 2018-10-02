882fe25b705e263ae26e5304b7e8d18463db1cdc
Chris Wilkinson <chriswilkinson84@gmail.com>
2018-10-02 13:43:49 +0100

    Allow multi-line method chaining (#7)
    * Allow multi-line method chaining
    
    * CS fix
    
    * Live with the weird namespace

---
+++ src/Libero/Sniffs/WhiteSpace/ObjectStaticOperatorSpacingSniff.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Libero\CodingStandard\Libero\Sniffs\WhiteSpace;
+
+use PHP_CodeSniffer\Standards\Squiz\Sniffs\WhiteSpace\ObjectOperatorSpacingSniff as BaseObjectOperatorSpacingSniff;
+use const T_DOUBLE_COLON;
+
+// This is only present to allow double colons to have separate configuration to the regular object operator.
+
+final class ObjectStaticOperatorSpacingSniff extends BaseObjectOperatorSpacingSniff
+{
+    public function register()
+    {
+        return [T_DOUBLE_COLON];
+    }
+}
