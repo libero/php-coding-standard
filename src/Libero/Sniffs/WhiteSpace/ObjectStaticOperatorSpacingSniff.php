ea9107988dc561654901e1f5a0e67d328894b677
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-11-22 15:07:59 +0000

    CS fix

--- src/Libero/Sniffs/WhiteSpace/ObjectStaticOperatorSpacingSniff.php
+++ src/Libero/Sniffs/WhiteSpace/ObjectStaticOperatorSpacingSniff.php
@@ -8,7 +8,6 @@ use PHP_CodeSniffer\Standards\Squiz\Sniffs\WhiteSpace\ObjectOperatorSpacingSniff
 use const T_DOUBLE_COLON;
 
 // This is only present to allow double colons to have separate configuration to the regular object operator.
-
 final class ObjectStaticOperatorSpacingSniff extends BaseObjectOperatorSpacingSniff
 {
     public function register()
